ac4ca037bc3b79ba827e0ee8b0680f0e2981d109
Kloudytics <kloudytics@gmail.com>
2024-09-17 09:36:50 +0300

    Initial commit with Next.js, TailwindCSS, and ShadCN setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1>Vitality Hub Home</h1>
+      <h1 className="text-3xl text-white underline">Vitality Hub Home</h1>
+      <Button>Click me</Button>
+      <PatientForm />
     </div>
   );
 }
